bbe04ebf92ee71024c9af7e22ddb5c4b297fcd9e
Karina Ibarra <karinaibarrait@gmail.com>
2024-10-21 15:47:51 -0600

    se añadió funcionalidad al formulario

--- sources/footer/items-form/itemForm.js
+++ sources/footer/items-form/itemForm.js
@@ -1,47 +1,200 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const itemForm = document.getElementById("itemForm");
+    const imagenInput = document.getElementById("imagen");
+    const previewImage = document.getElementById("preview");
+    const clearStorageButton = document.getElementById("clearStorage");
+    const modifyItemButton = document.getElementById("modifyItemBtn");
+    const productsContainer = document.createElement("div");
+    productsContainer.classList.add("row", "mt-4");
+    itemForm.parentElement.appendChild(productsContainer);
+    let currentEditIndex = null;
+    let currentImageData = null;
+  
+    if (imagenInput) {
+      // Mostrar vista previa de la imagen al seleccionarla
+      imagenInput.addEventListener("change", function (event) {
+        const file = event.target.files[0];
+        if (file) {
+          const reader = new FileReader();
+          reader.onload = function (e) {
+            previewImage.src = e.target.result;
+            previewImage.style.display = "block";
+            currentImageData = e.target.result;
+          };
+          reader.readAsDataURL(file);
+        } else {
+          previewImage.src = "";
+          previewImage.style.display = "none";
+        }
+      });
+    }
+  
+    if (clearStorageButton) {
+        clearStorageButton.addEventListener("click", () => {
+          // Mostrar el modal para la contraseña
+          const passwordModal = new bootstrap.Modal(document.getElementById('passwordModal'));
+          passwordModal.show();
+      
+          // Agregar evento de confirmación
+          document.getElementById('confirmPasswordButton').onclick = () => {
+            const password = document.getElementById('passwordInput').value;
+            
+            if (password === '123456') {
+              localStorage.removeItem("productos");
+              showAlert("El almacenamiento ha sido limpiado correctamente.", "info");
+              passwordModal.hide(); // Cerrar el modal
+            } else {
+              showAlert("Contraseña incorrecta. No se realizó ninguna acción.", "danger");
+            }
+          };
+        });
+      }
+       
+  
+    if (modifyItemButton) {
+      // Mostrar productos almacenados al presionar el botón "Modificar Item"
+      modifyItemButton.addEventListener("click", () => {
+        productsContainer.innerHTML = "";
+        let productos = JSON.parse(localStorage.getItem("productos")) || [];
+  
+        if (productos.length > 0) {
+          productos.forEach((producto, index) => {
+            const productCard = document.createElement("div");
+            productCard.classList.add("col-md-6", "col-lg-4", "mb-4");
+            productCard.innerHTML = `
+              <div class="card">
+                <img src="${producto.imagen || 'https://via.placeholder.com/150'}" class="card-img-top" alt="${producto.nombre}" style="height: 349px; width: 100%; object-fit: cover;">
+                <div class="card-body product-info text-center pb-2">
+                  <p class="card-text product-category">${producto.categoria}</p>
+                  <h4 class="card-title product-name mt-3 mb-1"><span class="d-inline-block text-truncate" style="max-width: 250px;">${producto.nombre}</span></h4>
+                  ${producto.descripcion ? `<p class="card-text mb-2"><small class="d-inline-block text-truncate" style="max-width: 270px;">${producto.descripcion}</small></p>` : ''}
+                  <p class="card-text product-price">$${producto.precio} MXN</p>
+                </div>
+                <div class="card-footer d-flex justify-content-center gap-2 mb-3">
+                    <button type="button" class="btn btn-primary" onclick="editProduct(${index})">Editar</button>
+                    <button type="button" class="btn btn-primary" onclick="deleteProduct(${index})">Eliminar</button>
+                </div>
+              </div>
+            `;
 
-// Initialize a new ItemsController with currentId set to 0
-const itemsController = new ItemsController(0);
 
-// Select the New Item Form
-const newItemForm = document.querySelector('#newItemForm');
-
-// Add an 'onsubmit' event listener
-newItemForm.addEventListener('submit', (event) => {
-    // Prevent default action
-    event.preventDefault();
-
-    // Select the inputs
-    const newItemCategory = document.querySelector('#newItemCategory');
-    const newItemNAme = document.querySelector('#newItemName');
-    const newItemDescription = document.querySelector('#newItemDescription');  
-    const newItemPrice = document.querySelector('#newItemPrice');    
-    const newItemSize = document.querySelector('#newItemSize');    
-    const newItemColor = document.querySelector('#newItemColor');    
-    const newItemWeight = document.querySelector('#newItemWeight');    
-    const newItemStock = document.querySelector('#newItemStock');    
-    const newItemURLImage = document.querySelector('#newItemURLImage');    
-    const newItemID = document.querySelector('#newItemID');    
-
-    // Get the values of the inputs
-    const category = newItemCategory.value;
-    const name = newItemNAme.value;
-    const description = newItemDescription.value;
-    const price = newItemPrice.value;
-    const size = newItemSize.value;
-    const color = newItemColor.value;
-    const weight = newItemWeight.value;
-    const stock = newItemStock.value;
-    const URLImage = newItemURLImage.value;
-    const ID = newItemID.value;
-
-    /*
-        Validation code here
-    */
-
-    // Add the item to the ItemsController
-    itemsController.addItem(category, name, description, price, size, color, weight, stock, URLImage, ID);
-
-    // Clear the form
-    newItemName.value = '';
-    newItemDescription.value = '';    
-});
+            productsContainer.appendChild(productCard);
+          });
+        } else {
+          productsContainer.innerHTML = '<p class="text-center">No hay productos disponibles para modificar.</p>';
+        }
+      });
+    }
+  
+    if (itemForm) {
+      itemForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+  
+        let form = event.target;
+        if (form.checkValidity()) {
+          const imageFile = imagenInput.files[0];
+  
+          const saveItem = (imageData) => {
+            const item = {
+              categoria: document.getElementById("categoria").value,
+              nombre: document.getElementById("nombre").value,
+              descripcion: document.getElementById("descripcion").value,
+              precio: document.getElementById("precio").value,
+              talla: document.getElementById("talla").value ? document.getElementById("talla").value.split(",") : [],
+              peso: document.getElementById("peso").value ? document.getElementById("peso").value.split(",") : [],
+              color: document.getElementById("color").value,
+              stock: document.getElementById("stock").value,
+              imagen: imageData || currentImageData || "https://via.placeholder.com/150"
+            };
+  
+            try {
+              let storedItems = JSON.parse(localStorage.getItem("productos")) || [];
+              if (currentEditIndex !== null) {
+                storedItems[currentEditIndex] = item;
+                currentEditIndex = null;
+                currentImageData = null;
+              } else {
+                storedItems.push(item);
+              }
+              localStorage.setItem("productos", JSON.stringify(storedItems));
+  
+              // Mostrar mensaje de éxito
+              showAlert("El item ha sido creado/modificado exitosamente.", "success");
+  
+              // Redirigir a la página de productos después de un pequeño retraso
+              setTimeout(() => window.location.href = "/sources/items/items.html", 1500);
+            } catch (e) {
+              console.error("Error al guardar en localStorage: ", e);
+              showAlert("No se pudo crear/modificar el producto debido a un problema de almacenamiento.", "danger");
+            }
+          };
+  
+          if (imageFile) {
+            const reader = new FileReader();
+            reader.onloadend = function () {
+              saveItem(reader.result);
+            };
+            reader.readAsDataURL(imageFile);
+          } else {
+            saveItem();
+          }
+        } else {
+          form.classList.add("was-validated");
+          showAlert("Por favor, complete todos los campos obligatorios.", "danger");
+        }
+      });
+    }
+  
+    function showAlert(message, type) {
+      const alertContainer = document.getElementById("alertContainer");
+      alertContainer.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                                      ${message}
+                                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                   </div>`;
+    }
+  
+    // Función para editar un producto existente
+    window.editProduct = function (index) {
+      let productos = JSON.parse(localStorage.getItem("productos")) || [];
+      const producto = productos[index];
+      if (producto) {
+        document.getElementById("categoria").value = producto.categoria;
+        document.getElementById("nombre").value = producto.nombre;
+        document.getElementById("descripcion").value = producto.descripcion;
+        document.getElementById("precio").value = producto.precio;
+        document.getElementById("talla").value = producto.talla.join(",");
+        document.getElementById("peso").value = producto.peso.join(",");
+        document.getElementById("color").value = producto.color;
+        document.getElementById("stock").value = producto.stock;
+        previewImage.src = producto.imagen;
+        previewImage.style.display = "block";
+        currentEditIndex = index;
+        currentImageData = producto.imagen;
+      }
+    };
+  
+ // Función para eliminar un producto existente
+window.deleteProduct = function (index) {
+    // Mostrar el modal para la contraseña
+    const passwordModal = new bootstrap.Modal(document.getElementById('passwordModal'));
+    passwordModal.show();
+  
+    // Agregar evento de confirmación
+    document.getElementById('confirmPasswordButton').onclick = () => {
+      const password = document.getElementById('passwordInput').value;
+      
+      if (password === '123456') {
+        let productos = JSON.parse(localStorage.getItem("productos")) || [];
+        productos.splice(index, 1);
+        localStorage.setItem("productos", JSON.stringify(productos));
+        showAlert("El producto ha sido eliminado exitosamente.", "success");
+        modifyItemButton.click(); // Actualizar la lista de productos
+        passwordModal.hide(); // Cerrar el modal
+      } else {
+        showAlert("Contraseña incorrecta. No se realizó ninguna acción.", "danger");
+      }
+    };
+  };  
+  });
+  
--- sources/items/items.js
+++ sources/items/items.js
@@ -1,33 +1,150 @@
-fetch('/sources/items/items.json')
-    .then(response => response.json())
-    .then(products => {
-        const productsContainer = document.getElementById('products-container');
-
-        products.forEach(product => {
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtener el contenedor de productos
+    const productsContainer = document.getElementById('products-container');
+  
+    // Obtener los productos almacenados en localStorage
+    let productosLocalStorage = JSON.parse(localStorage.getItem('productos')) || [];
+  
+    // Obtener los productos del archivo JSON y combinar con los productos de localStorage
+    fetch('/sources/items/items.json')
+      .then(response => response.json())
+      .then(productosJSON => {
+        // Mapear las propiedades del JSON a las propiedades esperadas
+        const productosMapeados = productosJSON.map(producto => {
+          return {
+            categoria: producto.category,
+            nombre: producto.name,
+            descripcion: producto.description,
+            precio: producto.price,
+            talla: producto.size || [],
+            peso: producto.weight || [],
+            color: producto.color || '',
+            stock: producto.stock,
+            imagen: producto.image
+          };
+        });
+  
+        // Unir los productos del JSON con los del localStorage
+        const productos = productosMapeados.concat(productosLocalStorage);
+  
+        // Verificar si hay productos almacenados
+        if (productos.length > 0) {
+          // Recorrer cada producto y crear una tarjeta para mostrarlo
+          productos.forEach((producto) => {
             const productCard = document.createElement('div');
-            productCard.classList.add('col-md-6', 'col-lg-4' ,'mb-4');
+            productCard.classList.add('col-md-6', 'col-lg-4', 'mb-4');
             productCard.innerHTML = `
-                <div class="card">
-                    <img src="${product.image || 'https://via.placeholder.com/150'}" class="card-img-top" alt="${product.name}">
-                    <div class="card-body product-info text-center pb-2">
-                        <p class="card-text product-category">${product.category}</p>
-                        <h4 class="card-title product-name mt-3 mb-1"><span class="d-inline-block text-truncate" style="max-width: 250px;">${product.name}</span></h4>
-                        ${product.description ? `<p class="card-text mb-2"><small class="d-inline-block text-truncate" style="max-width: 270px;">${product.description}</small></p>` : ''}
-                        <p class="card-text product-price">$${product.price} MXN</p>
-                    </div>
-                    <div class="card-footer d-flex justify-content-evenly mb-3">
-                    <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-primary">Agregar a mi bolsa</button>
-                        <button type="button" class="btn btn-outline-primary"><i class="bi bi-heart-fill"></i></button>
-                      </div>
+              <div class="card">
+                <img src="${producto.imagen || 'https://via.placeholder.com/150'}" class="card-img-top" alt="${producto.nombre}" style="height: 349px; width: 100%; object-fit: cover;">
+                <div class="card-body product-info text-center pb-2">
+                  <p class="card-text product-category">${producto.categoria}</p>
+                  <h4 class="card-title product-name mt-3 mb-1"><span class="d-inline-block text-truncate" style="max-width: 250px;">${producto.nombre}</span></h4>
+                  ${producto.descripcion ? `<p class="card-text mb-2"><small class="d-inline-block text-truncate" style="max-width: 270px;">${producto.descripcion}</small></p>` : ''}
+                  <p class="card-text product-price">$${producto.precio} MXN</p>
                 </div>
+                <div class="card-footer d-flex justify-content-evenly mb-3">
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-primary">Agregar a mi bolsa</button>
+                    <button type="button" class="btn btn-outline-primary"><i class="bi bi-heart-fill"></i></button>
+                  </div>
                 </div>
+              </div>
             `;
             productsContainer.appendChild(productCard);
-        });
-    })
-    .catch(error => console.error('Error cargando los productos:', error));
-
-
-
+          });
+        } else {
+          // Mostrar un mensaje si no hay productos
+          productsContainer.innerHTML = '<p class="text-center">No hay productos disponibles.</p>';
+        }
+      })
+      .catch(error => {
+        console.error('Error cargando los productos:', error);
+        // Mostrar un mensaje si ocurre un error al cargar los productos
+        productsContainer.innerHTML = '<p class="text-center">Error cargando los productos.</p>';
+      });
+  
+    // Manejar la creación de un nuevo producto
+    const itemForm = document.getElementById('itemForm');
+    const imagenInput = document.getElementById('imagen');
+    const previewImage = document.getElementById('preview');
+  
+    if (imagenInput) {
+      // Mostrar vista previa de la imagen al seleccionarla
+      imagenInput.addEventListener('change', function (event) {
+        const file = event.target.files[0];
+        if (file) {
+          const reader = new FileReader();
+          reader.onload = function (e) {
+            previewImage.src = e.target.result;
+            previewImage.style.display = 'block';
+          };
+          reader.readAsDataURL(file);
+        } else {
+          previewImage.src = '';
+          previewImage.style.display = 'none';
+        }
+      });
+    }
+  
+    if (itemForm) {
+      itemForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+  
+        // Obtener los valores del formulario
+        const categoria = document.getElementById('categoria').value;
+        const nombre = document.getElementById('nombre').value;
+        const descripcion = document.getElementById('descripcion').value;
+        const precio = document.getElementById('precio').value;
+        const talla = document.getElementById('talla').value;
+        const peso = document.getElementById('peso').value;
+        const color = document.getElementById('color').value;
+        const stock = document.getElementById('stock').value;
+        const imagen = imagenInput.files.length > 0 ? URL.createObjectURL(imagenInput.files[0]) : 'https://via.placeholder.com/150';
+  
+        // Crear un objeto de producto
+        if (categoria && nombre && descripcion && precio && stock) {
+          const nuevoProducto = {
+            categoria,
+            nombre,
+            descripcion,
+            precio,
+            talla,
+            peso,
+            color,
+            stock,
+            imagen,
+          };
+  
+          // Obtener los productos existentes del localStorage
+          let productos = JSON.parse(localStorage.getItem('productos')) || [];
+  
+          // Agregar el nuevo producto a la lista de productos
+          productos.push(nuevoProducto);
+  
+          // Guardar la lista actualizada en localStorage
+          localStorage.setItem('productos', JSON.stringify(productos));
+  
+          // Mostrar mensaje de éxito
+          showAlert('El item ha sido creado exitosamente.', 'success');
+  
+          // Redirigir a la página de productos después de un pequeño retraso
+          setTimeout(() => window.location.href = '/sources/items/items.html', 1500);
+        } else {
+          // Mostrar mensaje de error si faltan campos obligatorios
+          showAlert('No se pudo crear el producto. Por favor complete todos los campos obligatorios.', 'danger');
+        }
+      });
+    }
+  
+    function showAlert(message, type) {
+      const alertContainer = document.createElement('div');
+      alertContainer.className = `alert alert-${type} alert-dismissible fade show`;
+      alertContainer.role = 'alert';
+      alertContainer.innerHTML = `
+        ${message}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      `;
+      document.body.prepend(alertContainer);
+    }
+  });
 
